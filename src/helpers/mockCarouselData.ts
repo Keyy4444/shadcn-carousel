9dea2230ea339737ad7538aa60c81f29af052dbe
Oleksii <oleksprokopchenko@gmail.com>
2024-12-12 16:46:57 +0200

    added images and created mock data for sliders

---
+++ src/helpers/mockCarouselData.ts
@@ -0,0 +1,25 @@
+import nature from "../../public/images/nature.png";
+import blank from "../../public/images/blank.png";
+import { CarouselElement } from "@/types/carouselElement";
+
+export const arrayWithMultipleElements: CarouselElement[] = [
+  {
+    id: 1,
+    img: blank,
+  },
+  {
+    id: 2,
+    img: blank,
+  },
+  {
+    id: 3,
+    img: blank,
+  },
+];
+
+export const arrayWithOneElement = [
+  {
+    id: 1,
+    img: nature,
+  },
+];
---
+++ src/types/carouselElement.ts
@@ -0,0 +1,6 @@
+import { StaticImageData } from "next/image";
+
+export type CarouselElement = {
+  id: number;
+  img: StaticImageData;
+};
